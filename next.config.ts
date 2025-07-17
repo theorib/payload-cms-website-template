a55542d5375cadec6bc33e053b4f90e74769171f
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-07-17 11:26:06 +0100

    ♻️ refactor: migrate next config from js to ts and update settings
    - update experimental flags
    - add compiler options
    - update image config

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,25 @@
 import { withPayload } from '@payloadcms/next/withPayload'
 
 import redirects from './redirects.js'
+import { type NextConfig } from 'next'
 
 const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
   ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
   : undefined || process.env.__NEXT_PRIVATE_ORIGIN || 'http://localhost:3000'
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig: NextConfig = {
+  compiler: {
+    // remove console.logs in production mode
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+  experimental: {
+    optimizePackageImports: ['lucide-react'],
+    browserDebugInfoInTerminal: true,
+    typedRoutes: true,
+  },
   images: {
     remotePatterns: [
-      ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
+      ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map(item => {
         const url = new URL(item)
 
         return {
@@ -19,8 +28,10 @@ const nextConfig = {
         }
       }),
     ],
+    formats: ['image/avif', 'image/webp'],
+    // deviceSizes: [220, 390, 430, 640, 768, 1080, 1280, 1920, 2048, 2560, 3840],
   },
-  webpack: (webpackConfig) => {
+  webpack: webpackConfig => {
     webpackConfig.resolve.extensionAlias = {
       '.cjs': ['.cts', '.cjs'],
       '.js': ['.ts', '.tsx', '.js', '.jsx'],
