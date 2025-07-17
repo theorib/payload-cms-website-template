2c2750e81bdeb20d34cbd9077b235add9b9ee4d7
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-07-17 16:23:11 +0100

    🐛 fix: resolve typescript and linting errors and get validated env vars

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,11 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 import redirects from './redirects.js'
 import { type NextConfig } from 'next'
+import { env } from '@/utilities/env'
 
 const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
   ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-  : undefined || process.env.__NEXT_PRIVATE_ORIGIN || 'http://localhost:3000'
+  : process.env.__NEXT_PRIVATE_ORIGIN || env.NEXT_PUBLIC_SERVER_URL
 
 const nextConfig: NextConfig = {
   compiler: {
@@ -19,25 +20,24 @@ const nextConfig: NextConfig = {
   },
   images: {
     remotePatterns: [
-      ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map(item => {
-        const url = new URL(item)
-
-        return {
-          hostname: url.hostname,
-          protocol: url.protocol.replace(':', ''),
-        }
-      }),
+      ...[NEXT_PUBLIC_SERVER_URL].map(item => ({
+        hostname: new URL(item).hostname,
+        protocol: new URL(item).protocol.replace(':', '') as 'http' | 'https',
+      })),
     ],
     formats: ['image/avif', 'image/webp'],
     // deviceSizes: [220, 390, 430, 640, 768, 1080, 1280, 1920, 2048, 2560, 3840],
   },
-  webpack: webpackConfig => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  webpack: (webpackConfig: any) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     webpackConfig.resolve.extensionAlias = {
       '.cjs': ['.cts', '.cjs'],
       '.js': ['.ts', '.tsx', '.js', '.jsx'],
       '.mjs': ['.mts', '.mjs'],
     }
 
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     return webpackConfig
   },
   reactStrictMode: true,
