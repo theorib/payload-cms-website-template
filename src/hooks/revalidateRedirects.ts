a42a1b31e124b175c053aa608f34b567d989c90e
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-07-17 21:08:48 +0100

    ✨ feat: add type safety to revalidateRedirects hook
    - ensure correct type for doc parameter

--- src/hooks/revalidateRedirects.ts
+++ src/hooks/revalidateRedirects.ts
@@ -1,11 +1,14 @@
 import type { CollectionAfterChangeHook } from 'payload'
+import type { Redirect } from '../payload-types'
 
 import { revalidateTag } from 'next/cache'
 
 export const revalidateRedirects: CollectionAfterChangeHook = ({ doc, req: { payload } }) => {
+  const redirectDoc = doc as Redirect
+  
   payload.logger.info(`Revalidating redirects`)
 
   revalidateTag('redirects')
 
-  return doc
+  return redirectDoc
 }
