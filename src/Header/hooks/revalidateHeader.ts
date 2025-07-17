11f448d2a3bb645470a71a82d3668266b23ace81
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-07-17 21:34:21 +0100

    ♻️ refactor(header): use header type and add headerDoc variable

--- src/Header/hooks/revalidateHeader.ts
+++ src/Header/hooks/revalidateHeader.ts
@@ -1,13 +1,16 @@
 import type { GlobalAfterChangeHook } from 'payload'
+import type { Header } from '../../payload-types'
 
 import { revalidateTag } from 'next/cache'
 
 export const revalidateHeader: GlobalAfterChangeHook = ({ doc, req: { payload, context } }) => {
+  const headerDoc = doc as Header
+  
   if (!context.disableRevalidate) {
     payload.logger.info(`Revalidating header`)
 
     revalidateTag('global_header')
   }
 
-  return doc
+  return headerDoc
 }
