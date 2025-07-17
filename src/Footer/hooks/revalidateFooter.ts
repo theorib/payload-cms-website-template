0e5740ca47736d2d029aa92277ac90bde310605d
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-07-17 21:34:28 +0100

    ♻️ refactor(footer): use footer type and add footerDoc variable

--- src/Footer/hooks/revalidateFooter.ts
+++ src/Footer/hooks/revalidateFooter.ts
@@ -1,13 +1,16 @@
 import type { GlobalAfterChangeHook } from 'payload'
+import type { Footer } from '../../payload-types'
 
 import { revalidateTag } from 'next/cache'
 
 export const revalidateFooter: GlobalAfterChangeHook = ({ doc, req: { payload, context } }) => {
+  const footerDoc = doc as Footer
+  
   if (!context.disableRevalidate) {
     payload.logger.info(`Revalidating footer`)
 
     revalidateTag('global_footer')
   }
 
-  return doc
+  return footerDoc
 }
